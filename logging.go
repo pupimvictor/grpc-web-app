638e9ba2f90be801f789ef7553476824c4ad131f
Victor Pupim <victor.pupim@nytimes.com>
2018-11-26 22:19:08 -0500

    initial ci

---
+++ logging.go
@@ -0,0 +1,73 @@
+package grpcwebapp
+
+import (
+	"context"
+	"fmt"
+	"encoding/json"
+)
+
+func (s *service) StartInputstream(ctx context.Context) (error) {
+	eventsInputStream := s.inputStream.Start()
+	var passthroughStream chan *Event
+	passthroughflag := false
+
+	//todo: check race conditions for this whole block
+	for {
+		select {
+		case msg := <-eventsInputStream:
+			fmt.Printf("%s", msg.Message())
+			var event *Event
+			json.Unmarshal(msg.Message(), event)
+
+			//todo store in ds
+			fmt.Printf("e: %+v\n", *event)
+
+			if passthroughflag {
+				if passthroughStream != nil {
+					passthroughStream <- event
+				}
+			}
+		case eventStream := <-s.PassthroughCh:
+			passthroughStream = eventStream
+			passthroughflag = true
+
+		case <-s.StopPassthroughCh:
+			passthroughflag = false
+			close(passthroughStream)
+			passthroughStream = nil
+		}
+	}
+
+	return nil
+}
+
+func (s *service) LoadEvents(ctx context.Context, loadEventsRequest *LoadEventsRequest) (*LoadEventsResponse, error) {
+	return &LoadEventsResponse{}, nil
+}
+
+func (s *service) StreamEvents(streamEventsRequest *StreamEventsRequest, streamEventsServer EventLogger_StreamEventsServer) error {
+	eventFilter := streamEventsRequest.GetFilter()
+	
+	eventsStream := make(chan *Event)
+	s.PassthroughCh<- eventsStream
+
+	select {
+	case event := <-eventsStream:
+		if applyFilter(eventFilter, event) {
+			streamEventResp := &StreamEventsResponse{
+				Event:event,
+			}
+			streamEventsServer.Send(streamEventResp)
+		}
+	}
+	return nil
+}
+
+func (s *service) StopStreaming(context.Context, *Void) (*Void, error) {
+	s.StopPassthroughCh<- true
+	return &Void{}, nil
+}
+
+func applyFilter(f *Filter, e *Event) bool {
+	return true
+}
---
+++ service.go
@@ -0,0 +1,83 @@
+package grpcwebapp
+
+import (
+	"net/http"
+
+	"github.com/NYTimes/gizmo/server"
+	"google.golang.org/grpc"
+
+	"context"
+	"github.com/NYTimes/gizmo/pubsub"
+	"cloud.google.com/go/datastore"
+)
+
+type (
+	// service will implement server.service and
+	// handle all requests to the server.
+	service struct {
+		inputStream        pubsub.Subscriber
+		ds                 *datastore.Client
+		PassthroughCh      chan chan *Event
+		StopPassthroughCh  chan bool
+	}
+	// Config is a struct to contain all the needed
+	// configuration for our service
+	Config struct {
+		Server *server.Config
+	}
+)
+
+// NewRPCService will instantiate a service
+// with the given configuration.
+func NewRPCService(cfg *Config) *service {
+	//todo init ds and p/s
+	return &service{
+
+
+	}
+}
+
+// Prefix returns the string prefix used for all endpoints within
+// this service.
+func (s *service) Prefix() string {
+	return "/svc"
+}
+
+// Service provides the service with a description of the
+// service to serve and the implementation.
+func (s *service) Service() (*grpc.ServiceDesc, interface{}) {
+	return &_EventLogger_serviceDesc, s
+}
+
+// Middleware provides an http.Handler hook wrapped around all requests.
+// In this implementation, we're using a GzipHandler middleware to
+// compress our responses.
+func (s *service) Middleware(h http.Handler) http.Handler {
+	return h
+}
+
+// ContextMiddleware provides a server.ContextHAndler hook wrapped around all
+// requests. This could be handy if you need to decorate the request context.
+func (s *service) ContextMiddleware(h server.ContextHandler) server.ContextHandler {
+	return h
+}
+
+// ContextEndpoints may be needed if your server has any non-RPC-able
+// endpoints. In this case, we have none but still need this method to
+// satisfy the server.service interface.
+func (s *service) ContextEndpoints() map[string]map[string]server.ContextHandlerFunc {
+	return map[string]map[string]server.ContextHandlerFunc{}
+}
+
+// JSONContextEndpoints is a listing of all endpoints available in the service.
+func (s *service) JSONEndpoints() map[string]map[string]server.JSONContextEndpoint {
+	return map[string]map[string]server.JSONContextEndpoint{
+		"/health": {
+			"GET": s.Health,
+		},
+	}
+}
+
+func (s *service) Health(ctx context.Context, r *http.Request) (int, interface{}, error) {
+	return http.StatusOK, "healthy", nil
+}
